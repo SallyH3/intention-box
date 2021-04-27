6cc3291669f179eb15eeae2dcf46ca27ec46fcee
Sally Haefling <sallyhaefling@gmail.com>
2021-04-27 17:16:40 -0600

    Add dotenv

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json()); // Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Used to parse URL-encoded bodies
@@ -12,10 +15,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://gotStuffToDo:Peaches1984!@cluster0.uxfpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
