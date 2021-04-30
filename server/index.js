6c9830c26057c55dde1ecc586a5708501c0fa96b
Sally Haefling <sallyhaefling@gmail.com>
2021-04-29 19:58:04 -0600

    Add Procfile

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.use('/intentions', intentionRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello to Intention Box')
+});
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
