0f76acd4ed119c6660aea0f18da6a7e0b6c07958
Sally Haefling <sallyhaefling@gmail.com>
2021-04-29 19:56:23 -0600

    Modify url to use deployed url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/intentions';
+const url = 'https://intention-box.herokuapp.com/intentions';
 
 export const fetchIntentions = () => axios.get(url);
 export const createIntention = (newIntention) => axios.post(url, newIntention);
